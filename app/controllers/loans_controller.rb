65baa0ba4870a2d6ed6b40c13fe01610960bb969
Jenny Klich <jennyklich@gmail.com>
2023-08-21 17:13:27 -0500

    Add changes

--- app/controllers/loans_controller.rb
+++ app/controllers/loans_controller.rb
@@ -1,5 +1,4 @@
 class LoansController < ActionController::API
-
   rescue_from ActiveRecord::RecordNotFound do |exception|
     render json: 'not_found', status: :not_found
   end
@@ -9,6 +8,41 @@ class LoansController < ActionController::API
   end
 
   def show
-    render json: Loan.find(params[:id])
+    loan = Loan.find(params[:id])
+    render json: loan.as_json.merge(outstanding_balance: loan.outstanding_balance)
+  end
+
+  def create_payment
+    loan = Loan.find(params[:loan_id])
+    amount = params[:amount].to_f
+    payment_date = params[:payment_date]
+
+    if amount <= 0
+      render json: { error: 'Payment amount must be greater than 0' }, status: :unprocessable_entity
+      return
+    end
+
+    if amount > loan.outstanding_balance
+      render json: { error: 'Payment amount exceeds outstanding balance' }, status: :unprocessable_entity
+      return
+    end
+
+    payment = Payment.new(loan: loan, amount: amount, payment_date: payment_date)
+
+    if payment.save
+      render json: payment, status: :created
+    else
+      render json: { errors: payment.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def payments_index
+    loan = Loan.find(params[:loan_id])
+    render json: loan.payments
+  end
+
+  def show_payment
+    payment = Payment.find(params[:id])
+    render json: payment
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   resources :loans, defaults: {format: :json}
+  resources :loans do
+    member do
+      post 'payments', to: 'loans#create_payment'
+      get 'payments', to: 'loans#payments_index'
+    end
+    resources :payments, only: [:show], controller: 'loans'
+  end
+
 end
